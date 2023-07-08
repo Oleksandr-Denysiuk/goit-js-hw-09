2f5e99744c5a782e9c3ca06920b90c56d0856a47
Oleksandr-Denysiuk <saniarok4@gmail.com>
2023-07-08 18:39:59 +0200

    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', startSwitcher);
+stopBtn.addEventListener('click', stopSwitcher);
+
+let isActive = false;
+let intervalId = null;
+
+function startSwitcher() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopSwitcher() {
+  clearInterval(intervalId);
+  isActive = false;
+}
