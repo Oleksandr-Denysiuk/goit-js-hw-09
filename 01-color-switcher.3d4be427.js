22215ff0d8c2c5ecdbdb02fc7e1abc7a925f8896
Oleksandr-Denysiuk <117781425+Oleksandr-Denysiuk@users.noreply.github.com>
2023-07-08 16:40:31 +0000

    Deploying to gh-pages from @ Oleksandr-Denysiuk/goit-js-hw-09@2f5e99744c5a782e9c3ca06920b90c56d0856a47 🚀

---
+++ 01-color-switcher.3d4be427.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){if(o)return;o=!0,r=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(r),o=!1}));let o=!1,r=null;
+//# sourceMappingURL=01-color-switcher.3d4be427.js.map
---
+++ 01-color-switcher.cfc14bba.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){if(o)return;o=!0,r=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(r),o=!1}));var o=!1,r=null}();
+//# sourceMappingURL=01-color-switcher.cfc14bba.js.map
